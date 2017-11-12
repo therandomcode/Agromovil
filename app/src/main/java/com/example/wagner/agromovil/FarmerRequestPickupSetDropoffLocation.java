290f1fd4b200a2fa54319c2e07abe24ebfd92036
Kris Wagner <kriswagner.ch@gmail.com>
2017-11-12 17:27:48 -0500

    Clean commit

---
+++ app/src/main/java/com/example/wagner/agromovil/FarmerRequestPickupSetDropoffLocation.java
@@ -0,0 +1,13 @@
+package com.example.wagner.agromovil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FarmerRequestPickupSetDropoffLocation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_farmer_request_pickup_set_dropoff_location);
+    }
+}
